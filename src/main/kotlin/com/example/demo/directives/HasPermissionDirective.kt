70e28aa60fd25040d40c9a64e112f88d9a6596ab
Maxim Savenko <maxim.savenko@gmail.com>
2021-09-15 11:00:42 +0300

    ...

---
+++ src/main/kotlin/com/example/demo/directives/HasPermissionDirective.kt
@@ -0,0 +1,39 @@
+package com.example.demo.directives
+
+import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsRuntimeWiring
+import graphql.schema.GraphQLInputObjectField
+import graphql.schema.idl.RuntimeWiring
+import graphql.schema.idl.SchemaDirectiveWiring
+import graphql.schema.idl.SchemaDirectiveWiringEnvironment
+import java.time.LocalDateTime
+
+@DgsComponent
+class ResetValueDirective : SchemaDirectiveWiring {
+    companion object {
+        private const val NAME = "resetOnValue"
+        private const val ARGUMENT_NAME = "value"
+    }
+
+    @DgsRuntimeWiring
+    fun runtimeWiring(builder: RuntimeWiring.Builder): RuntimeWiring.Builder {
+        return builder.directiveWiring(ResetValueDirective())
+    }
+
+    override fun onInputObjectField(environment: SchemaDirectiveWiringEnvironment<GraphQLInputObjectField>): GraphQLInputObjectField {
+        val field = environment.element
+        if (field.getDirective(NAME) == null) {
+            return field
+        }
+        val parentType = environment.fieldsContainer
+
+        // add necessary transformation logic for the input field here
+
+        // (!) The lambda is called on start only
+        return field.transform { builder: GraphQLInputObjectField.Builder ->
+            builder
+                .defaultValue("It's new value ${LocalDateTime.now()}")
+                .build()
+        }
+    }
+}
